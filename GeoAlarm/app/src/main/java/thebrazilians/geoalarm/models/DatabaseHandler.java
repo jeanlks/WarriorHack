6c8ec7f9a190a447e0385fc0331ca846221faac4
augustosds3 <augustosd3@gmail.com>
2016-04-02 13:33:17 -0400

    DatabaseHandler
    Commit 1

--- GeoAlarm/app/src/main/java/thebrazilians/geoalarm/models/DatabaseHandler.java
+++ GeoAlarm/app/src/main/java/thebrazilians/geoalarm/models/DatabaseHandler.java
@@ -1,5 +1,6 @@
 package thebrazilians.geoalarm.models;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.DatabaseErrorHandler;
 import android.database.sqlite.SQLiteDatabase;
@@ -42,4 +43,27 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         super.onDowngrade(db, oldVersion, newVersion);
     }
+
+    public void addActivity(Activity activity){
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(KEY_NAME, activity.getTitle());
+        values.put(KEY_NAME, activity.getDescription());
+        values.put(KEY_NAME, activity.getDate());
+
+        db.insert(TABLE_ACTIVITY, null, values);
+        db.close();
+    }
+
+    public Activity getActivity(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.query(TABLE_ACTIVITY, new String[] {KEY_ID ,KEY_TITLE, KEY_DESCRIPTION, KEY_DATE}, KEY_ID + "=?", new String [] {String.valueOf(id)}, null, null, null, null);
+
+        if(cursor != null)
+            cursor.moveToFirst();
+
+        Contact contact = new Contact(Integer.parseInt(cursor.getString(0)), cursor.getString(1),
+    }
+
 }
